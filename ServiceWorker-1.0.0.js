d44c57d302834d833801e780bc984fb8f407df71
abc <anthonybouchereau@hotmail.com>
2021-02-08 10:33:10 +0100

    modifs

--- ServiceWorker-1.0.0.js
+++ ServiceWorker-1.0.0.js
@@ -1,6 +1,9 @@
+self.addEventListener('fetch', event => {
 	event.respondWith(
 		caches.open('umanis-1.0.0').then(cache => {
 			return cache.match(event.request).then(response => {
+				
+				//si on est offline, on utilise le cache
 				if (navigator.onLine === false) {
 				  return response;
 				}
@@ -14,6 +17,7 @@
 	);
 });
 
+// à l'installation de l'app, on met en cache les ressources nécessaires
 var urlsToCache = ["pwa.html","pwa-1.0.0.css","logo-umanis-blanc.svg"];
 
 self.addEventListener('install', function(event) {
